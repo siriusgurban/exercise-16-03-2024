c27fbd6deb42adc72844a57325d18c43df5311ba
2883857@student.edu.az <2883857@student.edu.az>
2024-03-16 21:03:47 +0400

    github users

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState("");
   const [data, setData] = useState([]);
+  const [dataGit, setDataGit] = useState([]);
 
   async function handleSearch() {
     const res = await fetch(
@@ -14,6 +17,19 @@ function App() {
     console.log(data.Search, "data");
     setData(data.Search);
   }
+
+  useEffect(() => {
+    axios({
+      url: "https://api.github.com/users",
+      method: "GET",
+    })
+      .then((res) => {
+        setDataGit(res);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
 
   return (
     <>
@@ -33,6 +49,22 @@ function App() {
             <img src={item.Poster} alt={item} />
           </div>
         ))}
+      </div>
+
+      <hr />
+
+      <div>
+        {console.log(dataGit, "dataGit")}
+        <h1>Git Hub Users</h1>
+        <div style={{ display: "flex", flexWrap: "wrap", gap: 20 + "px" }}>
+          {dataGit?.data?.map((item, index) => (
+            <div style={{ border: 1 + "px solid white" }} key={index}>
+              <h2>{item.login}</h2>
+              <p>{item.type}</p>
+              <img src={item.avatar_url} alt={item} />
+            </div>
+          ))}
+        </div>
       </div>
     </>
   );
